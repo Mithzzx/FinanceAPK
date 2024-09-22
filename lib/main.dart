eafccc96a5fcd1659e76ba5eeac9c7db32145b19
Mithun Rajan <149712505+Mithzzx@users.noreply.github.com>
2024-09-22 16:50:42 +0530

    Weather app

--- lib/main.dart
+++ lib/main.dart
@@ -15,43 +15,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Currency Converter',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Converter'),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        secondaryHeaderColor: Colors.tealAccent,
+        // Add more custom dark theme properties here
+      ), // This sets the dark theme
+      themeMode: ThemeMode.dark,
+      home: const MyHomePage(title: 'Currency Converter'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -59,15 +42,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  double _amount = 0;
 
-<<<<<<< Updated upstream
-  TextEditingController _controller = TextEditingController();
-
-  void _clearText() {
-    _controller.clear();  // Clears the text in the TextField
-    _calculate("0");
-=======
   @override
   void initState() {
     super.initState();
@@ -97,19 +72,59 @@ class _MyHomePageState extends State<MyHomePage> {
   void _refresh()
   {
     return;
->>>>>>> Stashed changes
   }
 
-  @override
-  void dispose() {
-    _controller.dispose();  // Clean up the controller when the widget is disposed
-    super.dispose();
+  SizedBox _forecastCard(icon,temp,time)
+  {
+    return SizedBox(
+        width:100,
+        height: 120,
+        child: Card(
+          elevation:20,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                icon,
+                const SizedBox(height: 4,),
+                Text(temp,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20
+                  ),
+                ),
+                const SizedBox(height: 3,),
+                Text(time)
+              ],
+            ),
+          ),
+        )
+    );
   }
 
-  void _calculate(String usd) {
-    setState(() {
-      _amount = int.parse(usd)*83.99;
-    });
+  Padding _addInfoCard(image,name,info)
+  {
+    double iconSize= 50;
+
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+          children : [SizedBox(
+            width:iconSize,
+            height: iconSize,
+            child: image,
+          ),
+            const SizedBox(height: 12),
+            Text(name),
+            const SizedBox(height: 12),
+            Text(info,
+              style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500
+              ),)
+          ]
+      ),
+    );
   }
 
   @override
@@ -119,70 +134,17 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    OutlineInputBorder border = const OutlineInputBorder(
-        borderSide: BorderSide(
-          color: Color.fromARGB(166, 37, 44, 30),
-          style: BorderStyle.solid,
-        ),
-        borderRadius: BorderRadius.all(Radius.circular(40))
-    );
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: AppBar(actions: [
+        IconButton(onPressed: _refresh, icon: const Icon(Icons.refresh_sharp))
+      ],
+        title: const Text("Weather",
+          style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.w600
+          ),
+        ),
       ),
-<<<<<<< Updated upstream
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.only(bottom: 20),
-              child: Text("₹${_amount.toStringAsFixed(2)}",
-                style: const TextStyle(
-                    fontSize: 75,
-                    fontWeight: FontWeight.w500,
-                    color: Color.fromARGB(192, 34, 43, 26)
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(20.0),
-              child: TextField(
-                controller: _controller,
-                keyboardType: TextInputType.number,
-                style: const TextStyle(
-                    fontSize: 15
-                ),
-                decoration: InputDecoration(
-                    hintText: "Enter the amount in USD",
-                    filled: true,
-                    fillColor: const Color.fromARGB(119, 239, 240, 228),
-                    focusedBorder: border,
-                    enabledBorder: border,
-                    prefixIcon: const Icon(Icons.monetization_on,
-                        color: Color.fromARGB(192, 34, 43, 26)
-                    )
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Access the value of the TextField using the controller
-                String enteredText = _controller.text;
-                _calculate(enteredText);
-                // You can also display it in the UI, show an alert, or store it in a variable
-              },
-              child: Text('Convert'),
-            ),
-          ],
-=======
       body:Container(
         padding: const EdgeInsets.all(20),
         child: SingleChildScrollView(
@@ -296,19 +258,10 @@ class _MyHomePageState extends State<MyHomePage> {
               )
             ],
           ),
->>>>>>> Stashed changes
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _clearText,
-        tooltip: 'Clear',
-        child: const Icon(Icons.clear),
-      ),
-    );// This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
 }
-<<<<<<< Updated upstream
-=======
 
 
->>>>>>> Stashed changes
